a857c71c2f56e9c9479b3c2186f283e63597813c
Will Hui <will@willhui.com>
2016-09-20 21:04:40 +0800

    init

---
+++ app.js
@@ -0,0 +1,1 @@
+var app = angular.module('hackerNews', ['hackerNews.controllers'])
---
+++ controllers/newsCtrl.js
@@ -0,0 +1,16 @@
+angular.module('hackerNews.controllers', [])
+  .controller('newsCtrl', ['$scope', '$http', function($scope, $http) {
+
+  var getNews = function() {
+
+    $http
+      .get('http://hn.algolia.com/api/v1/search_by_date?query=angular')
+      .then(function(resp){
+        console.log(resp.data)
+        $scope.news = resp.data
+      })
+  }
+
+  getNews()
+
+}])
